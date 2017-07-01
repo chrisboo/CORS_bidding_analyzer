2d6273d99ca62f55ec850cc8d48b5eba28e9050c
Christopher Boo <christopherboo66@gmail.com>
2017-07-02 00:26:00 +0800

    function to extract all pages that contains bidding data

---
+++ source.py
@@ -0,0 +1,10 @@
+import bs4 as bs
+import urllib.request
+
+def getSource(url):
+    source = urllib.request.urlopen(url).read()
+    soup = bs.BeautifulSoup(source, 'lxml')
+
+    urls = soup.find_all('a', href=True)
+
+    return [url.get('href') for url in urls if url.get('href').startswith('./Archive')]
