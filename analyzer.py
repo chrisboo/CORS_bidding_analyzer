9270c1bbb1b829d3fa351f3cc4f63b165b9ac68c
Christopher Boo <christopherboo66@gmail.com>
2017-07-02 00:01:15 +0800

    Print modules with highest lowest successful bids

---
+++ analyzer.py
@@ -0,0 +1,28 @@
+import bs4 as bs
+import pandas as pd
+import urllib.request
+
+sauce = urllib.request.urlopen('http://www.nus.edu.sg/cors/Archive/201617_Sem1/successbid_1A_20162017s1.html').read()
+soup = bs.BeautifulSoup(sauce, 'lxml')
+
+table = soup.table
+table_rows = soup.find_all('tr')
+
+data = []
+
+for tr in table_rows:
+ 	td = tr.find_all('td')
+ 	row = [i.text for i in td]
+
+ 	if len(row) < 9:
+ 		row = [data[-1][0]] + [data[-1][1]] + row[1:]
+
+ 	data.append(row)
+
+df = pd.DataFrame.from_records(data[2:], columns=data[0])
+
+df = df.apply(pd.to_numeric, errors='ignore')
+
+df = df.sort_values('LowestSuccBid', ascending=False)
+
+print(df.head(10).ix[:, ['Module', 'LowestSuccBid']])
