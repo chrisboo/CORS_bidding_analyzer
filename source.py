e89ad067f62cbadafb84486377e150fe8258da94
Christopher Boo <christopherboo66@gmail.com>
2017-07-02 08:38:01 +0800

    add function to filter webpages with successbid info

--- source.py
+++ source.py
@@ -1,10 +1,13 @@
 import bs4 as bs
 import urllib.request
 
+def usefulSource(url):
+    return url.startswith('./Archive') and "successbid" in url.get('href');
+
 def getSource(url):
     source = urllib.request.urlopen(url).read()
     soup = bs.BeautifulSoup(source, 'lxml')
 
     urls = soup.find_all('a', href=True)
 
-    return [url.get('href') for url in urls if url.get('href').startswith('./Archive') and "successbid" in url.get('href')]
+    return [url.get('href') for url in urls if usefulSource(url.get('href'))]
